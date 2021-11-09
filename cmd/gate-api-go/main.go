e9f399ce4d1b856478f4127eb65a2f94abf40553
Volodymyr Burdeinyi <burdeinyi.v.o@gmail.com>
2021-11-09 02:01:02 +0200

    + added function for keeping connection opened.

--- cmd/gate-api-go/main.go
+++ cmd/gate-api-go/main.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 	"github.com/posipaka-trade/gate-api-go/pkg/gate"
 	"github.com/posipaka-trade/posipaka-trade-cmn/exchangeapi"
-	"github.com/posipaka-trade/posipaka-trade-cmn/exchangeapi/order"
 	"github.com/posipaka-trade/posipaka-trade-cmn/exchangeapi/symbol"
 	"os"
+	"time"
 )
 
 func main() {
@@ -14,20 +14,54 @@ func main() {
 		Key:    os.Args[1],
 		Secret: os.Args[2],
 	})
-	result, err := mgr.SetOrder(order.Parameters{
-		Assets: symbol.Assets{
-			Base:  "XRP",
-			Quote: "USDT"},
-		Side:     order.Buy,
-		Type:     order.Limit,
-		Quantity: 4,
-		Price:    2,
-	})
-	fmt.Println(result)
-	balance, err := mgr.GetAssetBalance("USDT")
-	fmt.Println(balance)
+	defer mgr.Finish()
 
-	if err != nil {
-		panic(err.Error())
-	}
+	time.Sleep(5 * time.Second)
+
+	startTime := time.Now()
+	_, _ = mgr.GetCurrentPrice(symbol.Assets{
+		Base:  "ETH",
+		Quote: "USDT",
+	})
+	fmt.Println(time.Since(startTime).String())
+
+	time.Sleep(time.Second)
+	startTime = time.Now()
+	_, _ = mgr.GetCurrentPrice(symbol.Assets{
+		Base:  "ETH",
+		Quote: "USDT",
+	})
+	fmt.Println(time.Since(startTime).String())
+
+	time.Sleep(time.Second)
+	startTime = time.Now()
+	_, _ = mgr.GetCurrentPrice(symbol.Assets{
+		Base:  "ETH",
+		Quote: "USDT",
+	})
+	fmt.Println(time.Since(startTime).String())
+
+	time.Sleep(time.Second)
+	startTime = time.Now()
+	_, _ = mgr.GetCurrentPrice(symbol.Assets{
+		Base:  "ETH",
+		Quote: "USDT",
+	})
+	fmt.Println(time.Since(startTime).String())
+
+	time.Sleep(time.Second)
+	startTime = time.Now()
+	_, _ = mgr.GetCurrentPrice(symbol.Assets{
+		Base:  "ETH",
+		Quote: "USDT",
+	})
+
+	time.Sleep(110 * time.Second)
+	startTime = time.Now()
+	_, _ = mgr.GetCurrentPrice(symbol.Assets{
+		Base:  "ETH",
+		Quote: "USDT",
+	})
+
+	fmt.Println(time.Since(startTime).String())
 }
--- pkg/gate/gate_structs.go
+++ pkg/gate/gate_structs.go
@@ -4,6 +4,8 @@ import (
 	"github.com/posipaka-trade/posipaka-trade-cmn/exchangeapi"
 	"github.com/posipaka-trade/posipaka-trade-cmn/exchangeapi/order"
 	"net/http"
+	"sync"
+	"time"
 )
 
 const baseUrl = "https://api.gateio.ws"
@@ -12,13 +14,36 @@ type ExchangeManager struct {
 	apiKey exchangeapi.ApiKey
 
 	client *http.Client
+
+	wg        sync.WaitGroup
+	isWorking bool
 }
 
 func New(key exchangeapi.ApiKey) *ExchangeManager {
-	return &ExchangeManager{
+	mgr := &ExchangeManager{
 		apiKey: key,
-		client: &http.Client{},
+		client: &http.Client{
+			Transport: http.DefaultTransport,
+		},
+		isWorking: true,
 	}
+
+	mgr.wg.Add(1)
+	go func() {
+		defer mgr.wg.Done()
+		for mgr.isWorking {
+			_, _ = mgr.client.Get(baseUrl + prefix + "/")
+			time.Sleep(75 * time.Second)
+		}
+	}()
+
+	return mgr
+}
+
+// Finish completes inner goroutines
+func (manager *ExchangeManager) Finish() {
+	manager.isWorking = false
+	manager.wg.Wait()
 }
 
 var orderSideAlias = map[order.Side]string{
