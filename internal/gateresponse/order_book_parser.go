9475f5593e01ac3dee35bdf9fd91a6af271333b5
Oleg Lysenko <baryn72@gmail.com>
2021-11-17 21:43:09 +0200

    edited ask bid structs

--- internal/gateresponse/order_book_parser.go
+++ internal/gateresponse/order_book_parser.go
@@ -32,8 +32,7 @@ func ParseOrderBook(response *http.Response) (trade.OrderBook, error) {
 //TODO: Make parallel asks and bids parsing
 func asksAndBidsParse(asks, bids []interface{}) (trade.OrderBook, error) {
 	var orderBook trade.OrderBook
-	var ask trade.Ask
-	var bid trade.Bid
+	var askAndBid trade.AskAndBids
 	var err error
 
 	for i := 0; i < len(asks); i++ {
@@ -49,15 +48,15 @@ func asksAndBidsParse(asks, bids []interface{}) (trade.OrderBook, error) {
 		if isOk != true {
 			return trade.OrderBook{}, errors.New("[gateresponse] -> error when casting order book ask quantity")
 		}
-		ask.Price, err = strconv.ParseFloat(priceStr, 64)
+		askAndBid.Price, err = strconv.ParseFloat(priceStr, 64)
 		if err != nil {
 			return trade.OrderBook{}, errors.New("[gateresponse] -> error when parsing order book ask price to float64")
 		}
-		ask.Quantity, err = strconv.ParseFloat(quantityStr, 64)
+		askAndBid.Quantity, err = strconv.ParseFloat(quantityStr, 64)
 		if err != nil {
 			return trade.OrderBook{}, errors.New("[gateresponse] -> error when parsing order book ask quantity to float64")
 		}
-		orderBook.Asks = append(orderBook.Asks, ask)
+		orderBook.Asks = append(orderBook.Asks, askAndBid)
 	}
 	for i := 0; i < len(bids); i++ {
 		bidsI, isOk := bids[i].([]interface{})
@@ -72,15 +71,15 @@ func asksAndBidsParse(asks, bids []interface{}) (trade.OrderBook, error) {
 		if isOk != true {
 			return trade.OrderBook{}, errors.New("[gateresponse] -> error when casting order book bid quantity")
 		}
-		bid.Price, err = strconv.ParseFloat(priceStr, 64)
+		askAndBid.Price, err = strconv.ParseFloat(priceStr, 64)
 		if err != nil {
 			return trade.OrderBook{}, errors.New("[gateresponse] -> error when parsing order book bid price to float64")
 		}
-		bid.Quantity, err = strconv.ParseFloat(quantityStr, 64)
+		askAndBid.Quantity, err = strconv.ParseFloat(quantityStr, 64)
 		if err != nil {
 			return trade.OrderBook{}, errors.New("[gateresponse] -> error when parsing order book bid quantity to float64")
 		}
-		orderBook.Bids = append(orderBook.Bids, bid)
+		orderBook.Bids = append(orderBook.Bids, askAndBid)
 	}
 	return orderBook, nil
 }
--- pkg/gate/trade/order_book.go
+++ pkg/gate/trade/order_book.go
@@ -1,14 +1,10 @@
 package trade
 
 type OrderBook struct {
-	Asks []Ask
-	Bids []Bid
+	Asks []AskAndBids
+	Bids []AskAndBids
 }
-type Ask struct {
-	Price    float64
-	Quantity float64
-}
-type Bid struct {
+type AskAndBids struct {
 	Price    float64
 	Quantity float64
 }
